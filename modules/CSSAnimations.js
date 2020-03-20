833d316c87eb70c19db8ff6ccf7f2f7650f0b9be
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2020-03-20 09:16:41 -0700

    Create CSSAnimations.js

---
+++ modules/CSSAnimations.js
@@ -0,0 +1,9 @@
+import { ModemCore } from '../modem.js';
+
+class CSSAnimator extends ModemCore {
+	constructor(){
+		this.Name = "CSSAnimator";
+		this.Version = 1;
+		this.Description = "";
+	}
+}
